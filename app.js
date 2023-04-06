4264b81e77eff56e51d4d0c7439602c281feccd4
SharmiShriS <sharmishri06@gmail.com>
2023-04-06 11:41:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if(this.scrollY > 200){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+var typed=new Typed(".typing-1",{
+    strings:[
+        "Developer","Coder",
+        "Designer","Speaker"
+    ],
+    typeSpeed:80,
+    backSpeed:60,
+    loop:true
+});
+
+
+var typed=new Typed(".typing-2",{
+    strings:[
+        "Developer","Coder",
+        "Designer","Speaker"
+    ],
+    typeSpeed:80,
+    backSpeed:60,
+    loop:true
+});
+
+$('.carousel').owlCarousel({
+    margin:20,
+    loop:true,
+    navigation:true,
+    autoplay:true,
+    autoTimeOut:1000,
+    autoplayHoverPause:true,
+
+    responsive:{
+        0:{
+            items:1,
+            nav: false
+        },
+        600:{
+            items:2,
+            nav: false
+        },
+        1000:{
+            items:3,
+            nav: false
+        }
+    }
+
+})
+
+});
